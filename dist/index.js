aa5c6e560ef68da5892be65e20a60fe342cef43b
Arun Roberts <arunplaysbass@gmail.com>
2022-01-26 19:22:22 +1100

    built scaffolding. experimenting with JS implementation

---
+++ dist/index.js
@@ -0,0 +1,2 @@
+var mainTitle = document.getElementsByTagName('h1')[0];
+mainTitle.addEventListener('click', function () { return mainTitle.style.color = 'blue'; });
---
+++ index.ts
@@ -0,0 +1,2 @@
+let mainTitle: HTMLHeadingElement = document.getElementsByTagName('h1')[0]
+mainTitle.addEventListener('click', () => mainTitle.style.color = 'blue')
