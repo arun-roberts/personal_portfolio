6e43d6b60c9178d78c2cf525d4e682f9d6a4302a
Arun Roberts <arunplaysbass@gmail.com>
2022-01-27 16:30:34 +1100

    added scroll div

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,8 @@
 var mainTitle = document.getElementsByTagName('h1')[0];
-mainTitle.addEventListener('click', function () { return mainTitle.style.color = 'blue'; });
+var scrollDiv = document.getElementById('scrollDiv');
+for (var i = 0; i < 30; i++) {
+    var addItUp = scrollDiv.innerHTML + "<p class=\"iCantSeeYou\">0</p><br>";
+    scrollDiv.innerHTML = addItUp;
+}
+var testSubject = document.getElementById('testSubject');
+scrollDiv.addEventListener('scroll', function () { return console.log(scrollDiv.scrollTop.toString()); });
--- index.ts
+++ index.ts
@@ -1,2 +1,8 @@
 let mainTitle: HTMLHeadingElement = document.getElementsByTagName('h1')[0]
-mainTitle.addEventListener('click', () => mainTitle.style.color = 'blue')
+const scrollDiv: HTMLElement = document.getElementById('scrollDiv')
+for(let i = 0; i < 30; i++){
+    let addItUp = scrollDiv.innerHTML + `<p class="iCantSeeYou">0</p><br>`
+    scrollDiv.innerHTML = addItUp
+}
+const testSubject: HTMLElement = document.getElementById('testSubject')
+scrollDiv.addEventListener('scroll', () => console.log(scrollDiv.scrollTop.toString()))
