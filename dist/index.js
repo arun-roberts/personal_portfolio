22f11ebb486944585ac8740aa907d1e7bfc7aa58
Arun Roberts <arunplaysbass@gmail.com>
2022-04-14 12:57:58 +1000

    Added a style game

--- dist/index.js
+++ dist/index.js
@@ -1,15 +1,42 @@
-var mainTitle = document.getElementsByTagName('h1')[0];
-var scrollDiv = document.getElementById('scrollDiv');
-for (var i = 0; i < 30; i++) {
-    var addItUp = scrollDiv.innerHTML + "<p class=\"iCantSeeYou\">0</p><br>";
-    scrollDiv.innerHTML = addItUp;
-}
-var testSubject = document.getElementById('testSubject');
-var scrollNum;
-scrollDiv.addEventListener('scroll', function () {
-    var scroller = scrollDiv.scrollTop;
-    scrollNum = scroller;
-    console.log(scrollNum > 100);
-    scrollNum > 100 ? mainTitle.style.color = 'blue' : mainTitle.style.color = 'black';
-    scrollNum > 300 ? mainTitle.style.backgroundColor = 'green' : mainTitle.style.backgroundColor = 'white';
+var body = document.body;
+// ------------------ELEMENTS-----------------
+var me = document.getElementsByClassName('me')[0];
+var meHeading = document.getElementsByClassName('me__heading')[0];
+var work = document.getElementsByClassName('work')[0];
+var workHeading = document.getElementsByClassName('work__heading')[0];
+var profiles = document.getElementsByClassName('profiles')[0];
+var profilesHeading = document.getElementsByClassName('profiles__heading')[0];
+var education = document.getElementsByClassName('education')[0];
+var educationHeading = document.getElementsByClassName('education__heading')[0];
+var other = document.getElementsByClassName('other')[0];
+var otherHeading = document.getElementsByClassName('other__heading')[0];
+// -----------------BUTTONS-----------------
+var spacingButton = document.getElementById('spacing');
+var layoutButton = document.getElementById('layout');
+var darkButton = document.getElementsByClassName('dark-button')[0];
+var floridButton = document.getElementById('flord');
+var articles = Array.prototype.filter.call(document.getElementsByTagName('article'), function (a) { return !a.classList.contains('contact'); }), svgs = document.getElementsByTagName('svg'), buttons = document.getElementsByTagName('button'), workIcons = work.getElementsByTagName('svg');
+meHeading.addEventListener('click', function () {
+    me.classList.toggle('me---open');
+});
+workHeading.addEventListener('click', function () {
+    work.classList.toggle('work---open');
+});
+profilesHeading.addEventListener('click', function () {
+    profiles.classList.toggle('profiles---open');
+});
+educationHeading.addEventListener('click', function () {
+    education.classList.toggle('education---open');
+});
+otherHeading.addEventListener('click', function () {
+    other.classList.toggle('other---open');
+});
+spacingButton.addEventListener('click', function () {
+    body.classList.toggle('spacing');
+});
+layoutButton.addEventListener('click', function () {
+    body.classList.toggle('layout');
+});
+darkButton.addEventListener('click', function () {
+    body.classList.toggle('dark');
 });
--- index.ts
+++ index.ts
@@ -1,15 +1,47 @@
-let mainTitle: HTMLHeadingElement = document.getElementsByTagName('h1')[0]
-const scrollDiv: HTMLElement = document.getElementById('scrollDiv')
-for(let i = 0; i < 30; i++){
-    let addItUp = scrollDiv.innerHTML + `<p class="iCantSeeYou">0</p><br>`
-    scrollDiv.innerHTML = addItUp
-}
-const testSubject: HTMLElement = document.getElementById('testSubject')
-let scrollNum: number
-scrollDiv.addEventListener('scroll', () => {
-    let scroller = scrollDiv.scrollTop
-    scrollNum = scroller
-    console.log(scrollNum > 100)
-    scrollNum > 100 ? mainTitle.style.color = 'blue' : mainTitle.style.color = 'black'
-    scrollNum > 300 ? mainTitle.style.backgroundColor = 'green' : mainTitle.style.backgroundColor = 'white'
+const body: HTMLElement = document.body
+// ------------------ELEMENTS-----------------
+const me: Element = document.getElementsByClassName('me')[0]
+const meHeading: Element = document.getElementsByClassName('me__heading')[0]
+const work: Element = document.getElementsByClassName('work')[0]
+const workHeading: Element = document.getElementsByClassName('work__heading')[0]
+const profiles: Element = document.getElementsByClassName('profiles')[0]
+const profilesHeading: Element = document.getElementsByClassName('profiles__heading')[0]
+const education: Element = document.getElementsByClassName('education')[0]
+const educationHeading: Element = document.getElementsByClassName('education__heading')[0]
+const other: Element = document.getElementsByClassName('other')[0]
+const otherHeading: Element = document.getElementsByClassName('other__heading')[0]
+// -----------------BUTTONS-----------------
+const spacingButton: Element = document.getElementById('spacing')
+const layoutButton: Element = document.getElementById('layout')
+const darkButton: Element = document.getElementsByClassName('dark-button')[0]
+const floridButton: Element = document.getElementById('flord')
+let articles = Array.prototype.filter.call(document.getElementsByTagName('article'), (a: HTMLElement) => !a.classList.contains('contact')),   
+    svgs = document.getElementsByTagName('svg'), 
+    buttons = document.getElementsByTagName('button'),
+    workIcons = work.getElementsByTagName('svg')
+
+meHeading.addEventListener('click', () => {
+    me.classList.toggle('me---open')
 })
+workHeading.addEventListener('click', () => {
+    work.classList.toggle('work---open')
+})
+profilesHeading.addEventListener('click', () => {
+    profiles.classList.toggle('profiles---open')
+})
+educationHeading.addEventListener('click', () => {
+    education.classList.toggle('education---open')
+})
+otherHeading.addEventListener('click', () => {
+    other.classList.toggle('other---open')
+})
+spacingButton.addEventListener('click', () => {
+    body.classList.toggle('spacing')
+})
+layoutButton.addEventListener('click', () => {
+    body.classList.toggle('layout')
+})
+darkButton.addEventListener('click', () => {
+    body.classList.toggle('dark')
+})
+
