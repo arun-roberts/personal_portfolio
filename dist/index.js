6c6e3878123f4834588fe4582e7d13d1b1b4b86d
Arun Roberts <arunplaysbass@gmail.com>
2022-04-15 19:45:54 +1000

    Refining style game and adding popups.

--- dist/index.js
+++ dist/index.js
@@ -10,33 +10,76 @@ var education = document.getElementsByClassName('education')[0];
 var educationHeading = document.getElementsByClassName('education__heading')[0];
 var other = document.getElementsByClassName('other')[0];
 var otherHeading = document.getElementsByClassName('other__heading')[0];
+var styleGames = document.getElementById('style-games');
 // -----------------BUTTONS-----------------
-var spacingButton = document.getElementById('spacing');
 var layoutButton = document.getElementById('layout');
-var darkButton = document.getElementsByClassName('dark-button')[0];
-var floridButton = document.getElementById('flord');
+var darkButton = document.getElementById('dark');
+var floridButton = document.getElementById('florid');
 var articles = Array.prototype.filter.call(document.getElementsByTagName('article'), function (a) { return !a.classList.contains('contact'); }), svgs = document.getElementsByTagName('svg'), buttons = document.getElementsByTagName('button'), workIcons = work.getElementsByTagName('svg');
+//---------------FUNCTIONS-----------------------
+var putItIn = function (s) {
+    var popUp = document.createElement('modal'), count = s.split(' ').length;
+    popUp.classList.add('modal');
+    popUp.innerHTML = "<p>" + s + "</p>";
+    body.appendChild(popUp);
+    setTimeout(function () {
+        popUp.classList.add('modal---visible');
+        setTimeout(function () {
+            popUp.classList.remove('modal---visible');
+            setTimeout(function () { return body.removeChild(popUp); }, 800);
+        }, 600 * count);
+    }, 100);
+};
 meHeading.addEventListener('click', function () {
-    me.classList.toggle('me---open');
+    !body.classList.contains('florid') &&
+        me.classList.toggle('me---open');
 });
 workHeading.addEventListener('click', function () {
-    work.classList.toggle('work---open');
+    !body.classList.contains('florid') &&
+        work.classList.toggle('work---open');
 });
 profilesHeading.addEventListener('click', function () {
-    profiles.classList.toggle('profiles---open');
+    !body.classList.contains('florid') &&
+        profiles.classList.toggle('profiles---open');
 });
 educationHeading.addEventListener('click', function () {
-    education.classList.toggle('education---open');
+    !body.classList.contains('florid') &&
+        education.classList.toggle('education---open');
 });
 otherHeading.addEventListener('click', function () {
-    other.classList.toggle('other---open');
-});
-spacingButton.addEventListener('click', function () {
-    body.classList.toggle('spacing');
+    !body.classList.contains('florid') &&
+        other.classList.toggle('other---open');
 });
 layoutButton.addEventListener('click', function () {
     body.classList.toggle('layout');
+    if (body.classList.contains('layout')) {
+        putItIn('Yes. Order.');
+    }
+    else {
+        putItIn('A chaos enthusiast, are we?');
+    }
 });
 darkButton.addEventListener('click', function () {
     body.classList.toggle('dark');
+    darkButton.innerHTML = body.classList.contains('dark') ? 'LIGHT' : 'DARK';
+    if (body.classList.contains('dark')) {
+        putItIn('Oooooo, edgy.');
+    }
+    else {
+        putItIn('Sure, back to normal. Is your life as exciting as your choices?');
+    }
+});
+floridButton.addEventListener('click', function () {
+    body.classList.toggle('florid');
+    floridButton.innerHTML = body.classList.contains('florid') ? 'Dull' : 'Florid';
+    if (body.classList.contains('florid')) {
+        putItIn('So fancy.');
+    }
+    else {
+        putItIn('Really???');
+    }
+});
+window.addEventListener('scroll', function () {
+    var s = window.scrollY / 300;
+    styleGames.style.opacity = s < 1 ? String(s) : '1';
 });
--- index.ts
+++ index.ts
@@ -10,38 +10,78 @@ const education: Element = document.getElementsByClassName('education')[0]
 const educationHeading: Element = document.getElementsByClassName('education__heading')[0]
 const other: Element = document.getElementsByClassName('other')[0]
 const otherHeading: Element = document.getElementsByClassName('other__heading')[0]
+const styleGames: HTMLElement = document.getElementById('style-games')
 // -----------------BUTTONS-----------------
-const spacingButton: Element = document.getElementById('spacing')
 const layoutButton: Element = document.getElementById('layout')
-const darkButton: Element = document.getElementsByClassName('dark-button')[0]
-const floridButton: Element = document.getElementById('flord')
+const darkButton: Element = document.getElementById('dark')
+const floridButton: Element = document.getElementById('florid')
 let articles = Array.prototype.filter.call(document.getElementsByTagName('article'), (a: HTMLElement) => !a.classList.contains('contact')),   
     svgs = document.getElementsByTagName('svg'), 
     buttons = document.getElementsByTagName('button'),
     workIcons = work.getElementsByTagName('svg')
+//---------------FUNCTIONS-----------------------
+const putItIn = (s: string) => {
+    let popUp = document.createElement('modal'),
+        count = s.split(' ').length
+    popUp.classList.add('modal')
+    popUp.innerHTML = `<p>${s}</p>`
+    body.appendChild(popUp)
+    setTimeout(() => {
+        popUp.classList.add('modal---visible')
+        setTimeout(() => {
+            popUp.classList.remove('modal---visible')
+            setTimeout(() => body.removeChild(popUp), 800)
+        }, 600 * count)
+    }, 100)
+}
 
 meHeading.addEventListener('click', () => {
+    !body.classList.contains('florid') &&
     me.classList.toggle('me---open')
 })
 workHeading.addEventListener('click', () => {
+    !body.classList.contains('florid') &&
     work.classList.toggle('work---open')
 })
 profilesHeading.addEventListener('click', () => {
+    !body.classList.contains('florid') &&
     profiles.classList.toggle('profiles---open')
 })
 educationHeading.addEventListener('click', () => {
+    !body.classList.contains('florid') &&
     education.classList.toggle('education---open')
 })
 otherHeading.addEventListener('click', () => {
+    !body.classList.contains('florid') &&
     other.classList.toggle('other---open')
-})
-spacingButton.addEventListener('click', () => {
-    body.classList.toggle('spacing')
 })
 layoutButton.addEventListener('click', () => {
     body.classList.toggle('layout')
+    if (body.classList.contains('layout')) {
+        putItIn('Yes. Order.')
+    } else {
+        putItIn('A chaos enthusiast, are we?')
+    }
 })
 darkButton.addEventListener('click', () => {
     body.classList.toggle('dark')
+    darkButton.innerHTML = body.classList.contains('dark') ? 'LIGHT' : 'DARK'
+    if (body.classList.contains('dark')) {
+        putItIn('Oooooo, edgy.')
+    } else {
+        putItIn('Sure, back to normal. Is your life as exciting as your choices?')
+    }
 })
-
+floridButton.addEventListener('click', () => {
+    body.classList.toggle('florid')
+    floridButton.innerHTML = body.classList.contains('florid') ? 'Dull' : 'Florid'
+    if (body.classList.contains('florid')) {
+        putItIn('So fancy.')
+    } else {
+        putItIn('Really???')
+    }
+})
+window.addEventListener('scroll', () => {
+    let s = window.scrollY / 300
+    styleGames.style.opacity = s < 1 ? String(s) : '1'
+})
